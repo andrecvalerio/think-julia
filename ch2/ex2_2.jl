394f1b66650794c3a627763ee7161b8e6d655336
Andre C.Valerio <andrecvalerio@gmail.com>
2020-12-01 11:27:33 -0300

    Create ex2_2.jl

---
+++ ch2/ex2_2.jl
@@ -0,0 +1,22 @@
+# Exercise 2-2
+# 1)
+42 = n 
+# Error: invalid assignment location 
+
+# 2)
+x = y = 1
+# Valid assignment
+
+# 3)
+x=y=1;
+# The command result is not printed when we end it with ;
+
+# 4) 
+c = 1.
+# If its a number, it will treat it as a float 
+
+# 5)
+xy
+# Error, undefined variable
+5x 
+# Valid operation 
