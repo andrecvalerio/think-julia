a4a7cef82e35a9626a5f0659bd5e510341b671e4
Andre C.Valerio <andrecvalerio@gmail.com>
2020-12-01 11:27:31 -0300

    Create ex2_1.jl

---
+++ ch2/ex2_1.jl
@@ -0,0 +1,7 @@
+# Type the following statements in the Juia REPL and see what they do
+5
+# Print the number 5
+x = 5
+# Assign the number 5 to variable x and print it
+x + 1
+# Add one to the variable x and print it 
