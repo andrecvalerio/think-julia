c5041cf44fcfcf65df2d42866f851cb26c911493
Andre C.Valerio <andrecvalerio@gmail.com>
2020-12-01 10:38:21 -0300

    Create ex1_1.jl

---
+++ ch1/ex1_1.jl
@@ -0,0 +1,29 @@
+# Exercise 1-1
+# 1)
+println("Testing print function")
+println("Testing print function"
+# When we leave out one parentheses the REPL keep waiting for the next command and dont execute until we close 
+# the statement or exit the script
+println "Testing print function"
+# When we leave out both parentheses the REPL throws a syntax error
+
+# 2)
+println("Testing print function)
+# Throws an error of incomplete syntax 
+
+println(Testing print function)
+# Throws an error of missing argument
+
+# 3)
++2
+# Nothing happens
+2++2
+# Error of undefined operator
+
+# 4)
+02
+# Ignore the leading zero
+
+# 5)
+2 2
+# Error of extra token after end of expression 
