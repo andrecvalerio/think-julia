937a962220c51824ce71d9a3f9901d7cfb8d364e
Andre C.Valerio <andrecvalerio@gmail.com>
2020-12-02 12:47:01 -0300

    Create ex4_1.jl

---
+++ ch4/ex4_1.jl
@@ -0,0 +1,15 @@
+# Exercise 4-1
+using ThinkJulia
+
+🐢 = Turtle()
+
+@svg begin
+    forward(🐢, 100)    
+    turn(🐢,-90)
+    forward(🐢,100)
+    turn(🐢,-90)
+    forward(🐢,100)
+    turn(🐢,-90)
+    forward(🐢,100)
+end
+
